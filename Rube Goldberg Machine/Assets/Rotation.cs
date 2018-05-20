a43cac4c347dd836cb017eff7b2dcc91499953c0
Borislav98 <antikovb@gmail.com>
2018-05-20 21:17:44 +0100

    add

---
+++ Rube Goldberg Machine/Assets/Rotation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+    public float speed = 100000f;
+    public bool rot = false;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "BallTrigger") ;
+        {
+            rot =  true;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(rot == true )
+        transform.Rotate(0, 2, 0);
+	}
+}
---
+++ Rube Goldberg Machine/Assets/Rotation2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation2 : MonoBehaviour
+{
+    public bool rot = false;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "BallTrigger")
+        {
+            rot = true;
+        }
+
+        else if (collision.gameObject.name == "BallTrigger2")
+        {
+            rot = false;
+        }
+
+
+    }
+    
+
+
+    void Start()
+    {
+
+    }
+
+ 
+    void Update()
+    {
+        while (rot == true)
+            transform.Rotate(0, 2, 0);
+        
+    }
+}
