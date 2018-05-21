648e4df93d41ca2875820c67151ac94abbf98a7e
Borislav98 <antikovb@gmail.com>
2018-05-21 02:59:34 +0100

    add

---
+++ Rube Goldberg Machine/Assets/Kinematic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kinematic : MonoBehaviour {
+    private const bool V = false;
+    public GameObject Bal;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag=="Spinner")
+        {
+           Bal.GetComponent<Rigidbody>().isKinematic = V;
+        }
+    }
+}
