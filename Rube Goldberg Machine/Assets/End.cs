19645ef565545ed4f651058875fcdb1fe78ce31c
Borislav98 <antikovb@gmail.com>
2018-05-21 05:34:02 +0100

    Add

---
+++ Rube Goldberg Machine/Assets/End.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class End : MonoBehaviour {
+
+    public GameObject EndRemote;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Spinner")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+    }
+}
---
+++ Rube Goldberg Machine/Assets/Knematic2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knematic2 : MonoBehaviour {
+
+    private const bool V = false;
+    public GameObject Bouncy;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Spinner")
+        {
+            Bouncy.GetComponent<Rigidbody>().isKinematic = V;
+        }
+    }
+}
